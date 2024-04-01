a102e75669d35ebc5bc80164a133aa59ab45143d
chauttm <chauttm.vnu@gmail.com>
2024-04-01 16:39:50 +0700

    simplified flappy bird clone

---
+++ README.md
@@ -0,0 +1,13 @@
+# flappy
+Flappy Bird Clone simplified from https://github.com/phoemur/flappy
+
+# Requirements
+  - C++11
+
+  - SDL2
+
+  - SDL2_image
+
+  - SDL2_ttf
+
+  - SDL2_mixer
---
+++ music.h
@@ -0,0 +1,40 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include <SDL_mixer.h>
+#include <string>
+
+class Music {
+    Mix_Music *gMusic;
+public:
+    Music();
+    Music(const std::string& path);
+    ~Music() noexcept;
+
+    Music(const Music&) = delete;
+    Music& operator=(const Music&) = delete;
+    Music(Music&&) noexcept = default;
+    Music& operator=(Music&&) noexcept = default;
+
+    void play();
+    void pause();
+    void stop();
+};
+
+class Sound {
+    Mix_Chunk* gChunk;
+public:
+    Sound();
+    Sound(const std::string& path);
+    ~Sound() noexcept;
+
+    Sound(const Sound&) = delete;
+    Sound& operator=(const Sound&) = delete;
+    Sound(Sound&&) noexcept = default;
+    Sound& operator=(Sound&&) noexcept = default;
+
+    void play();
+};
+
+
+#endif // MUSIC_H
